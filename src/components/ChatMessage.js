8d0f576817e73d5ba3e7a6682a023657a21b3efa
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-12-05 14:36:48 +0600

    chatroom and chatmessage component

---
+++ src/components/ChatMessage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import firebase from "firebase/app";
+const auth = firebase.auth();
+
+const ChatMessage = (props) => {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+
+  return (
+    <>
+      <div className={`message ${messageClass}`}>
+        <img
+          src={
+            photoURL || "https://api.adorable.io/avatars/23/abott@adorable.png"
+          }
+        />
+        <p>{text}</p>
+      </div>
+    </>
+  );
+};
+
+export default ChatMessage;
